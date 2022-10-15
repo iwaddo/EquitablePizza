2b065113617f4fe62f79081d52957626b7d8b0d2
Ian Waddington <iwaddo@icloud.com>
2022-10-15 17:59:55 +0100

    Initial Commit

---
+++ EquitablePizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EquitablePizza
+//
+//  Created by Ian Waddington on 15/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
